4b2395c4b4e75e4c74828a28f23d8bd8f13ffe0c
guiamaralsbc <38924984+guiamaralsbc@users.noreply.github.com>
2019-02-21 11:41:44 -0300

    Arquivos Essenciais

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ManageExcel.UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtOrigem = new System.Windows.Forms.TextBox();
+            this.txtDestino = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnArquivo = new System.Windows.Forms.Button();
+            this.btnPasta = new System.Windows.Forms.Button();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.fbdOrigem = new System.Windows.Forms.FolderBrowserDialog();
+            this.ofdDestino = new System.Windows.Forms.OpenFileDialog();
+            this.txtPlanilha = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtOrigem
+            // 
+            this.txtOrigem.Location = new System.Drawing.Point(12, 31);
+            this.txtOrigem.Name = "txtOrigem";
+            this.txtOrigem.Size = new System.Drawing.Size(240, 20);
+            this.txtOrigem.TabIndex = 0;
+            // 
+            // txtDestino
+            // 
+            this.txtDestino.Location = new System.Drawing.Point(12, 70);
+            this.txtDestino.Name = "txtDestino";
+            this.txtDestino.Size = new System.Drawing.Size(240, 20);
+            this.txtDestino.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Arquivo de Origem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Diretório de Destino";
+            // 
+            // btnArquivo
+            // 
+            this.btnArquivo.Location = new System.Drawing.Point(258, 29);
+            this.btnArquivo.Name = "btnArquivo";
+            this.btnArquivo.Size = new System.Drawing.Size(33, 23);
+            this.btnArquivo.TabIndex = 4;
+            this.btnArquivo.Text = "...";
+            this.btnArquivo.UseVisualStyleBackColor = true;
+            this.btnArquivo.Click += new System.EventHandler(this.btnArquivo_Click);
+            // 
+            // btnPasta
+            // 
+            this.btnPasta.Location = new System.Drawing.Point(258, 67);
+            this.btnPasta.Name = "btnPasta";
+            this.btnPasta.Size = new System.Drawing.Size(33, 23);
+            this.btnPasta.TabIndex = 5;
+            this.btnPasta.Text = "...";
+            this.btnPasta.UseVisualStyleBackColor = true;
+            this.btnPasta.Click += new System.EventHandler(this.btnPasta_Click);
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(221, 185);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 44);
+            this.btnGerar.TabIndex = 6;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // ofdDestino
+            // 
+            this.ofdDestino.FileName = "openFileDialog1";
+            // 
+            // txtPlanilha
+            // 
+            this.txtPlanilha.Location = new System.Drawing.Point(12, 111);
+            this.txtPlanilha.Name = "txtPlanilha";
+            this.txtPlanilha.Size = new System.Drawing.Size(240, 20);
+            this.txtPlanilha.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nome da Planilha";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 241);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPlanilha);
+            this.Controls.Add(this.btnGerar);
+            this.Controls.Add(this.btnPasta);
+            this.Controls.Add(this.btnArquivo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDestino);
+            this.Controls.Add(this.txtOrigem);
+            this.Name = "Form1";
+            this.Text = "Teste OLEDB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtOrigem;
+        private System.Windows.Forms.TextBox txtDestino;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnArquivo;
+        private System.Windows.Forms.Button btnPasta;
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.FolderBrowserDialog fbdOrigem;
+        private System.Windows.Forms.OpenFileDialog ofdDestino;
+        private System.Windows.Forms.TextBox txtPlanilha;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ManageExcel.UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnArquivo_Click(object sender, EventArgs e)
+        {
+
+            if (ofdDestino.ShowDialog() == DialogResult.OK)
+            {
+                txtOrigem.Text = ofdDestino.FileName;
+            }
+        }
+
+        private void btnPasta_Click(object sender, EventArgs e)
+        {
+            if (fbdOrigem.ShowDialog() == DialogResult.OK)
+            {
+                txtDestino.Text = fbdOrigem.SelectedPath;
+            }
+
+        }
+
+        private bool Validar()
+        {
+            string origem = txtOrigem.Text;
+            string destino = txtDestino.Text;
+            string planilha = txtPlanilha.Text;
+
+            if (String.IsNullOrWhiteSpace(origem) || String.IsNullOrWhiteSpace(destino) || String.IsNullOrWhiteSpace(planilha))
+            {
+                MessageBox.Show("Não deixe nenhum campo em branco!");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            if (Validar())
+            {
+                string pathFrom = txtOrigem.Text;
+                string pathTo = String.Format(@"{0}\teste.xlsx", txtDestino.Text);
+                string sheetName = txtPlanilha.Text;
+                DataTable dt = ReadExcel(pathFrom, sheetName);
+                WriteExcel(dt, pathTo, sheetName);
+            }
+        }
+
+        private void WriteExcel(DataTable table, string path, string sheetName)
+        {
+            string connectionString = String.Empty;
+            try
+            {
+                connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended Properties=\"Excel 12.0 Xml; HDR=YES\";";
+                string fileName = Path.GetFileName(path);
+
+                CriarExcel criar = new CriarExcel();
+                CriarExcel.ExportToExcelOleDb(table, connectionString, fileName, true, sheetName);
+                MessageBox.Show("Planilha Gerada com Sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problemas na Geração do Arquivo");
+            }
+        }
+
+
+        private DataTable ReadExcel(string path, string sheetName)
+        {
+            try
+            {
+                using (OleDbConnection conn = new OleDbConnection())
+                {
+                    DataTable dt = new DataTable();
+                    conn.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";" + "Extended Properties='Excel 12.0 Xml;HDR=YES;IMEX=1;MAXSCANROWS=0'";
+                    using (OleDbCommand comm = new OleDbCommand())
+                    {
+                        comm.CommandText = "Select * from [" + sheetName + "$]";
+                        comm.Connection = conn;
+                        conn.Open();
+                        using (OleDbDataAdapter da = new OleDbDataAdapter())
+                        {
+                            da.SelectCommand = comm;
+                            da.Fill(dt);
+                            conn.Close();
+                            return dt;
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Problemas na Leitura do Arquivo");
+                return new DataTable();
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
